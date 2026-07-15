ff477b83087c07e2751fc58c9eb6e00545eeaf76
winred77731-boop <winred77731@gmail.com>
2026-07-15 10:52:49 -0500

    Restore four-column gallery layout

--- src/app/promotional-gallery/page.tsx
+++ src/app/promotional-gallery/page.tsx
@@ -251,7 +251,7 @@ export default function PromotionalGallery() {
               )}
             </div>
 
-            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-3">
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
               {section.images.map((image, imageIndex) => (
                 <motion.div
                   key={image}
